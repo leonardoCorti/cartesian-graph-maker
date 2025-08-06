789d725a517f4046bf4996ca3c9565bc5d0b7d43
Leonardo Corti <leonardo.filippo@ymail.com>
2025-08-06 15:35:59 +0200

    cli init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "synonymous_plotter")]
+#[command(about = "A program to create a graph from a database synonymous", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// server
+    Serve {},
+    /// graph operations
+    Graph {},
+    /// database operations
+    Db {
+        #[command(subcommand)]
+        action: Option<DbCommand>,
+    },
+}
+
+#[derive(Subcommand)]
+enum DbCommand {
+    Populate {},
+    Query {},
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command.unwrap_or(Commands::Serve {}) {
+        Commands::Serve {} => {
+            println!("server");
+        }
+        Commands::Graph {} => {
+            println!("graph operations");
+        }
+        Commands::Db { action: _ } => {
+            println!("Database operations");
+        }
+    }
 }
